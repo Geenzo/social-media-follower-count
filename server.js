26e2b19ffa15ecda68f7f8471d806723a2ddde30
Darren Hay <darrenhay1994@gmail.com>
2018-05-31 21:34:45 +0100

    setting up initial request for instagram

--- server.js
+++ server.js
@@ -6,10 +6,24 @@ const app = express()
 
 app.get('/scrape', function(req, res) {
 
+    // scraping from this url
+    let url = 'https://www.instagram.com/darrenhay1994/'
+    
+    request(url, function(error, response, html) {
+        
+        if(!error) {
+            let $ = cheerio.load(html)
+
+            let posts, followers, following
+            let json = { 
+                posts: "",
+                followers: "",
+                following: ""
+            }
+        }
+    })
 })
 
 app.listen('8081')
-
 console.log('Server started on port 8081')
-
 exports = module.exports = app
