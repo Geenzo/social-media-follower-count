9ff4cf9720e2aac6d8f8965342311dcbddab3655
Darren Hay <darrenhay1994@gmail.com>
2018-05-31 23:17:41 +0100

    updating script to get twitter tweets / followers and following numbers

--- server.js
+++ server.js
@@ -7,19 +7,36 @@ const app = express()
 app.get('/scrape', function(req, res) {
 
     // scraping from this url
-    let url = 'https://www.instagram.com/darrenhay1994/'
-    
+    let url = 'https://twitter.com/geenzo'
+
     request(url, function(error, response, html) {
+        console.log(`request sent to ${url}`);
         
         if(!error) {
             let $ = cheerio.load(html)
 
             let posts, followers, following
-            let json = { 
-                posts: "",
-                followers: "",
-                following: ""
+            let json = {
+                url: url,
             }
+            
+            
+            //this is html attribute for tweets / follows / following section
+            $('.ProfileNav-stat').filter(function() {
+                // storing filtered data
+                var data = $(this);
+                let twitterStatName = data.children().first().text() 
+                let twitterStateNumber = data.children().last().text()
+                let twitterStateNumberParsed = parseInt(twitterStateNumber.replace(/,/g,''), 10)
+                json[twitterStatName] = twitterStateNumberParsed
+
+                console.log(json);
+                
+            })
+
+        } else {
+            console.log('failed to fetch request');
+            
         }
     })
 })
