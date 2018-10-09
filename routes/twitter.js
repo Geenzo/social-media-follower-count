cfa9b6b92f0c95d8a62f8696de4e495865e11031
Darren Hay <darrenhay1994@gmail.com>
2018-10-09 22:20:19 +0100

    refator: changing twitter data captures from local file storage to mongodb

--- routes/twitter.js
+++ routes/twitter.js
@@ -1,7 +1,7 @@
 const request = require('request')
 const cheerio = require('cheerio')
-const fs = require('fs')
-const path = require('path');
+const { twitterSchema } = require('../model/twitter')
+const mongoose = require('mongoose')
 
 exports.scrapeTwitterFunc = function(req, res) {
 
@@ -9,42 +9,34 @@ exports.scrapeTwitterFunc = function(req, res) {
     let url = 'https://twitter.com/geenzo'
 
     request(url, function(error, response, html) {
+        if(error) console.log('failed to fetch request'); 
+        
         console.log(`request sent to ${url}`);
-
+        
         let json = {
             url: url,
         }
 
-        if(!error) {
-            let $ = cheerio.load(html)
-            
-            //this is html attribute for tweets / follows / following section
-            $('.ProfileNav-stat').filter(function() {
-                // storing filtered data
-                let data = $(this);
-                let twitterStatName = data.children().first().text() 
-                let twitterStateNumber = data.children().last().text()
-                let twitterStateNumberParsed = parseInt(twitterStateNumber.replace(/,/g,''), 10)
-                json[twitterStatName] = twitterStateNumberParsed
- 
-                let todaysDate = new Date()
-                json.captureDate = todaysDate
-            })
-
-        } else {
-            console.log('failed to fetch request'); 
-        }
-
-        var options = {year: 'numeric', month: 'long', day: 'numeric' }
-        let todaysDate = new Date().toLocaleDateString("en-GB", options)
+        let $ = cheerio.load(html)
         
-        let filePath = path.join(__dirname, `../output/twitter/${todaysDate}.json`)
-        
-        fs.writeFile(filePath, JSON.stringify(json, null, 4), function(err) {
-            if(err) {
-                throw Error('Error: error writing to filepath')
-            }
-            console.log('Twitter file successfully written! - Check your project directory for the output.json file')
+        //this is html attribute for tweets / follows / following section
+        $('.ProfileNav-stat').filter(function() {
+            // storing filtered data
+            let data = $(this);
+            let twitterStatName = data.children().first().text() 
+            let twitterStateNumber = data.children().last().text()
+            let twitterStateNumberParsed = parseInt(twitterStateNumber.replace(/,/g,''), 10)
+            json[twitterStatName] = twitterStateNumberParsed
+
+            let todaysDate = new Date()
+            json.captureDate = todaysDate
+        })
+
+        const twitterModel = mongoose.model('twitter', twitterSchema, 'twitter')
+        const newTwitterCapture = new twitterModel(json)
+        newTwitterCapture.save( err => {
+            if (err) throw Error('Error: saving twitter capture')
+            console.log('saved twitter account successfully')
         })
 
         res.send('Twitter Scraped! Check your console!')
