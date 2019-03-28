5961badf3c7a93553bb9e2b9ee117dca037cea48
Darren Hay <darrenhay1994@gmail.com>
2019-03-28 23:28:20 +0000

    update: react and mdb versions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
